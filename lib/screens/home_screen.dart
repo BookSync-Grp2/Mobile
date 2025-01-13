17fb3befb53a8f8eb9e295d51c0f944d10cb8a29
mikaellecam <lecam.mikael@gmail.com>
2025-01-13 21:43:32 +0100

    added auth service usage in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/screens/login_screen.dart';
+import 'package:mobile/services/auth_service.dart';
+import 'package:mobile/services/service_locator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,9 +13,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final authService = getIt<AuthService>();
+
+    if (authService.currentUser == null) {
+      return const LoginScreen();
+    }
+
     return Scaffold(
+      body: Text("HELLO"),
       backgroundColor: Colors.white,
-      child:
     );
   }
 }
