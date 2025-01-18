de311fd19caa9fda1fceb95512a31ae4356bfea7
mikaellecam <lecam.mikael@gmail.com>
2025-01-18 19:16:03 +0100

    added User fields

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,12 @@
 class User {
   final int id;
-  final String email;
   final String firstName;
   final String lastName;
-  final String token;
+  final String email;
+  final DateTime dateJoined;
+  final bool isValidated;
+  final String roleType;
+  final String? token;
 
   User({
     required this.id,
@@ -11,6 +14,9 @@ class User {
     required this.firstName,
     required this.lastName,
     required this.token,
+    required this.dateJoined,
+    required this.roleType,
+    required this.isValidated,
   });
 
   factory User.fromJson(Map<String, dynamic> json, String token) {
@@ -20,6 +26,9 @@ class User {
       firstName: json['firstName'],
       lastName: json['lastName'],
       token: token,
+      dateJoined: DateTime.parse(json['dateJoined']),
+      roleType: json['roleType'],
+      isValidated: json['validated'] ?? false,
     );
   }
 }
