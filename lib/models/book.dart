8a63b6788e8ec519795b1c3db3cdd79e9b5710c0
mikaellecam <lecam.mikael@gmail.com>
2025-01-20 21:56:55 +0100

    removed coverUrl

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -3,7 +3,6 @@ class Book {
   final String title;
   final String author;
   final String isbn;
-  final String coverUrl;
   final int publishedYear;
   final bool isAvailable;
 
@@ -12,7 +11,6 @@ class Book {
     required this.title,
     required this.author,
     required this.isbn,
-    required this.coverUrl,
     required this.publishedYear,
     required this.isAvailable,
   });
@@ -23,7 +21,6 @@ class Book {
       title: json['title'],
       author: json['author'],
       isbn: json['isbn'],
-      coverUrl: json['coverUrl'],
       publishedYear: json['publishedYear'],
       isAvailable: json['available'],
     );
